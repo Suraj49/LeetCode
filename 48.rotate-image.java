9cd604633966c64b9a96e56c929cf0101df300ad
Suraj49 <1507surajkumar@gmail.com>
2022-03-24 09:41:04 +0530

    commit

---
+++ 48.rotate-image.java
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=48 lang=java
+ *
+ * [48] Rotate Image
+ */
+
+// @lc code=start
+class Solution {
+
+    public void swap(int i, int j, int[][] arr) {
+        int temp = arr[i][j];
+        arr[i][j] = arr[j][i];
+        arr[j][i] = temp;
+    }
+
+    public void transpose(int[][] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                swap(i, j, arr);
+            }
+        }
+    }
+
+    public void reverse(int[] oneD) {
+        int i = 0;
+        int j = oneD.length - 1;
+        while (i < j) {
+            int t = oneD[i];
+            oneD[i] = oneD[j];
+            oneD[j] = t;
+            i++;
+            j--;
+        }
+    }
+
+    public void reverseRow(int[][] arr) {
+
+        for (int[] row : arr) {
+            reverse(row);
+        }
+
+    }
+
+    public void rotate(int[][] arr) {
+        transpose(arr);
+        reverseRow(arr);
+    }
+}
+// @lc code=end
