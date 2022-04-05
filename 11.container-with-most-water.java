a551ec7c68588c48157133fab3c1472f8635069a
Suraj49 <1507surajkumar@gmail.com>
2022-04-05 09:36:02 +0530

    commit

---
+++ 11.container-with-most-water.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=11 lang=java
+ *
+ * [11] Container With Most Water
+ */
+
+// @lc code=start
+class Solution {
+    public int maxArea(int[] H) {
+         int ans = 0, i = 0, j = H.length-1, res = 0;
+        while (i < j) {
+            if (H[i] <= H[j]) {
+                res = H[i] * (j - i);
+                i++;
+            } else {
+                res = H[j] * (j - i);
+                j--;
+            }
+            if (res > ans) ans = res;
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
