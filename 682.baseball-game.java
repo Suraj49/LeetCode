4469168e12da514430cebe2f968828d95522324f
Suraj49 <1507surajkumar@gmail.com>
2022-04-10 18:37:43 +0530

    commit

---
+++ 682.baseball-game.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=682 lang=java
+ *
+ * [682] Baseball Game
+ */
+
+// @lc code=start
+class Solution {
+    public int calPoints(String[] ops) {
+        Stack<Integer> stack = new Stack<>();
+    int sum = 0;
+    for (String op : ops) {
+      if (op.equals("+")) {
+        int prev = stack.pop();
+        int prevToPrev = stack.peek();
+        int newScore = prev + prevToPrev;
+        sum += newScore;
+        stack.push(prev);
+        stack.push(newScore);
+      }
+      else if (op.equals("C")) {
+        sum -= stack.pop();
+      }
+      else if (op.equals("D")) {
+        sum += stack.peek() * 2;
+        stack.push(stack.peek() * 2);
+      }
+      else {
+        stack.push(Integer.parseInt(op));
+        sum += stack.peek();
+      }
+    }
+    return sum;
+    }
+}
+// @lc code=end
+
