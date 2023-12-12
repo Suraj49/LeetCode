33f5d7c8a2303b253d0aa8096d3ada00d75b3c75
Suraj <1507surajkuamr@gmail.com>
2023-12-12 23:40:36 +0530

    comming leet code problem 171

---
+++ 171.excel-sheet-column-number.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=171 lang=java
+ *
+ * [171] Excel Sheet Column Number
+ */
+
+// @lc code=start
+class Solution {
+    public int titleToNumber(String s) {
+        int result = 0;
+        int p = 0;
+        for( int index = s.length() - 1; index >= 0; index--){
+            char c = s.charAt(index);
+            int val = (int)c - 65 + 1;
+            result += val * Math.pow(26, p);
+            p++;
+        }
+        return result;
+    }
+}
+// @lc code=end
+
